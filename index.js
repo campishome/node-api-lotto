33ef5fa1e70a57a9059f31d9a1eb515aa984ff6f
campishome <campnapat007@gmail.com>
2024-08-29 23:45:20 +0700

    fixed index

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 const express = require("express");
-const cors = require("cors");
-const loginRouter = require("./API/login").router; 
-const testRouter = require("./API/test").router;   
-const bodyParser = require("body-parser");
-
 const app = express();
-
 const PORT = 4000;
+
 app.listen(PORT, () => {
     console.log(`API LISTENING ON PORT ${PORT}`);
 });
-
-app.use(
-    cors({
-        origin: "*",
-    })
-);
+// const cors = require("cors");
+// const loginRouter = require("./API/login").router; 
+// const testRouter = require("./API/test").router;   
+// const bodyParser = require("body-parser");
 
 
-app.use(bodyParser.json());
-app.use("/", testRouter); 
-app.use("/login", loginRouter); 
+// app.use(
+//     cors({
+//         origin: "*",
+//     })
+// );
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my API UWU.');
+});
+
+// app.use(bodyParser.json());
+// app.use("/", testRouter); 
+// app.use("/login", loginRouter); 
 
 module.exports = app;

6fe6aef4831d9de357c990bfca9e06fb1aff01d1
campishome <campnapat007@gmail.com>
2024-08-29 23:55:47 +0700

    fixed index

--- API/login.js
+++ API/login.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Login API');
-});
+// router.get('/', (req, res) => {
+//     res.send('Login API');
+// });
 
-module.exports = { router };
+// module.exports = { router };
--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 const express = require("express");
 const app = express();
 const PORT = 4000;
+const db = require("./dbconnect");
+const cors = require("cors");
+// const bodyParser = require("body-parser");
+
+// const loginRouter = require("./API/login").router; 
+// const testRouter = require("./API/test").router;
+
+app.use(
+    cors({
+        origin: "*",
+    })
+);
 
 app.listen(PORT, () => {
     console.log(`API LISTENING ON PORT ${PORT}`);
 });
-// const cors = require("cors");
-// const loginRouter = require("./API/login").router; 
-// const testRouter = require("./API/test").router;   
-// const bodyParser = require("body-parser");
 
-
-// app.use(
-//     cors({
-//         origin: "*",
-//     })
-// );
-
+ 
 app.get('/', (req, res) => {
     res.send('Welcome to my API UWU.');
+});
+
+app.get('/users', async (req, res) => {
+    try {
+        const [rows] = await db.query('SELECT * FROM User');
+        res.json(rows);
+    } catch (error) {
+        res.status(500).send('Error fetching users');
+    }
 });
 
 // app.use(bodyParser.json());

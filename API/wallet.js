6c7acc30f63e8747e0a493669f914eb80f56ed85
campishome <campnapat007@gmail.com>
2024-09-08 23:49:20 +0700

    wallet fixed

--- API/wallet.js
+++ API/wallet.js
@@ -9,10 +9,33 @@ router.post('/add_money', async (req, res) => {
         const updateWallet = 'UPDATE Customer SET user_wallet = user_wallet + ? WHERE user_id = ?';
         await db.query(updateWallet, [amount,userId]);
 
-        res.status(201).json({ message: 'Add money successfully' });
+        res.status(201).json({ message: 'Deposit successfully' });
     } catch (error) {
-        console.error('Add money Error:', error.message); // Log error details
-        res.status(500).json({ message: 'Add money Error', error: error.message });
+        console.error('Deposit Error:', error.message); // Log error details
+        res.status(500).json({ message: 'Deposit Error', error: error.message });
+    }
+});
+
+router.post('/withdraw', async (req, res) => {
+    const { userId,amount } = req.body;
+
+    try {
+        const checkBalanceQuery = 'SELECT user_wallet FROM Customer WHERE user_id = ?';
+        const [user] = await db.query(checkBalanceQuery, [userId]);
+
+        const currentBalance = user.user_wallet;//-----*
+
+        if (amount > currentBalance) {
+            return res.status(400).json({ message: 'มันจะถอนไปด้ายยางงายวะ เงินมันมีนิ๊ดเดียวเองวะ โผมว่ามันถอนไปม่ายด้ายย' });
+        }
+
+        const updateWallet = 'UPDATE Customer SET user_wallet = user_wallet - ? WHERE user_id = ?';
+        await db.query(updateWallet, [amount,userId]);
+
+        res.status(201).json({ message: 'Withdraw successfully' });
+    } catch (error) {
+        console.error('Withdraw Error:', error.message); // Log error details
+        res.status(500).json({ message: 'Withdraw Error', error: error.message });
     }
 });
 

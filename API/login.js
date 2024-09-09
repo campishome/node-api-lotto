88f16e8d0e78c89cd5487f770e09b0517d4590f3
campishome <campnapat007@gmail.com>
2024-09-09 21:16:52 +0700

    all fixed

--- API/login.js
+++ API/login.js
@@ -4,7 +4,7 @@ const db = require("../dbconnect");
 
 router.get('/', async (req, res) => {
     try {
-        const [rows] = await db.query('SELECT user_id,user_name,user_phone,user_email,user_wallet,user_type FROM Customer WHERE user_type != "a"');
+        const [rows] = await db.query('SELECT user_id,user_name,user_email,user_wallet,user_type, user_image FROM Customer WHERE user_type != "a"');
         res.json(rows);
     } catch (error) {
         res.status(500).send('Error fetching users');
@@ -14,7 +14,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     try {
         const userId = req.params.id;
-        const [rows] = await db.query('SELECT user_id, user_name, user_phone, user_email, user_wallet, user_type, user_image FROM Customer WHERE user_id = ?', [userId]);
+        const [rows] = await db.query('SELECT user_id, user_name, user_email, user_wallet, user_type, user_image FROM Customer WHERE user_id = ?', [userId]);
         if (rows.length > 0) {
             res.json(rows[0]);
         } else {
@@ -28,11 +28,11 @@ router.get('/:id', async (req, res) => {
 
 
 router.post('/login', async (req, res) => {
-    const { phone, password } = req.body; // Destructuring phone and password from request body
+    const { email, password } = req.body; // Destructuring phone and password from request body
 
     try {
-        const query = 'SELECT user_id, user_type, user_name,user_phone,user_email FROM Customer WHERE user_phone = ? AND user_password = ?';
-        const [rows] = await db.query(query, [phone, password]);
+        const query = 'SELECT user_id, user_wallet, user_name,user_email,user_image,user_type FROM Customer WHERE user_email = ? AND user_password = ?';
+        const [rows] = await db.query(query, [email, password]);
 
         if (rows.length > 0) {
             res.json(rows[0]); // Return the first matching user
@@ -46,25 +46,25 @@ router.post('/login', async (req, res) => {
 });
 
 router.post('/register', async (req, res) => {
-    const { username, phone, email, password ,wallet} = req.body;
+    const { username, email, password} = req.body;
     const image = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png';
-    // Validate input data
-    if (!username || !phone || !email || !password || !wallet) {
-        return res.status(400).json({ message: "All fields (username, phone, email, password,wallet) are required" });
+    const wallet = 1000
+    if (!username  || !email || !password ) {
+        return res.status(400).json({ message: "All fields ( username, email, password) are required" });
     }
 
     try {
         // Check for existing user by phone or email
-        const checkQuery = 'SELECT user_id FROM Customer WHERE user_phone = ?';
-        const [existingUser] = await db.query(checkQuery, [phone]);
+        const checkQuery = 'SELECT user_id FROM Customer WHERE user_email = ?';
+        const [existingUser] = await db.query(checkQuery, [email]);
 
         if (existingUser.length > 0) {
             return res.status(400).json({ message: 'User already exists with this phone number or email' });
         }
 
         // Insert the new user
-        const insertQuery = 'INSERT INTO Customer (user_name, user_phone, user_email, user_password, user_type, user_wallet , user_image) VALUES (?, ?, ?, ?, ? ,?,?)';
-        await db.query(insertQuery, [username, phone, email, password, 'c',wallet, image]);
+        const insertQuery = 'INSERT INTO Customer (user_name, user_email, user_password, user_type, user_wallet , user_image) VALUES (?, ?, ?, ?,? ,?)';
+        await db.query(insertQuery, [username,email, password, 'c',wallet, image]);
 
         res.status(201).json({ message: 'User registered successfully' });
     } catch (error) {
--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const bodyParser = require("body-parser");
 
 const loginRouter = require("./API/login").router; 
 const testRouter = require("./API/test").router;
-const showRouter = require("./API/show").router;
 const lottoRouter = require("./API/lotto").router;
 const walletRouter = require("./API/wallet").router;
 
@@ -25,7 +24,6 @@ app.listen(PORT, () => {
 app.use(bodyParser.json());
 app.use("/", testRouter); 
 app.use("/users", loginRouter);
-app.use("/shows", showRouter);
 app.use("/lotto", lottoRouter);
 app.use("/wallet", walletRouter); 
 

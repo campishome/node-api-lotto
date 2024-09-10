24606acfbc3b0c9b478ec703d1b56e09f34af8c4
campishome <campnapat007@gmail.com>
2024-09-10 21:59:59 +0700

    random

--- API/lotto.js
+++ API/lotto.js
@@ -15,7 +15,6 @@ router.post('/createLotto', async (req, res) => {
     const { lottoNumber } = req.body;
 
     try {
-        // Check for existing user by phone or email
         const checkQuery = 'SELECT lotto_id FROM LottoAll WHERE lotto_number = ?';
         const [existingUser] = await db.query(checkQuery, [lottoNumber]);
         const lottoStatus = "ยังไม่ถูกซื้อ"
@@ -25,7 +24,6 @@ router.post('/createLotto', async (req, res) => {
             return res.status(400).json({ message: 'Lotto already exists' });
         }
 
-        // Insert the new user
         const insertQuery = 'INSERT INTO LottoAll (lotto_number, lotto_status) VALUES (?, ?)';
         await db.query(insertQuery, [lottoNumber,lottoStatus]);
 
@@ -40,7 +38,6 @@ router.post('/buyLotto', async (req, res) => {
     const { lottoId,userId } = req.body;
 
     try {
-        // Check for existing lottoId and userId
         const checkQueryLotto = 'SELECT lotto_number FROM LottoAll WHERE lotto_id = ?';
         const [existingLotto] = await db.query(checkQueryLotto, [lottoId]);
         const checkQueryUser = 'SELECT user_name FROM Customer WHERE user_id = ?';
@@ -51,8 +48,6 @@ router.post('/buyLotto', async (req, res) => {
             return res.status(400).json({ message: 'Lotto or User not exists' });
         }
 
-
-        // Insert the new user
         const insertQuery = 'INSERT INTO LottoBought (lotto_id, user_id) VALUES (?, ?)';
         await db.query(insertQuery, [lottoId,userId]);
 
@@ -65,6 +60,26 @@ router.post('/buyLotto', async (req, res) => {
     } catch (error) {
         console.error('Error creating lotto:', error.message); // Log error details
         res.status(500).json({ message: 'Purchase Error', error: error.message });
+    }
+});
+
+const generateUniqueLottoNumbers = () => { //create random number  
+    const lottoNumbers = new Set();
+
+    while (lottoNumbers.size < 100) {
+        const number = Math.floor(100000 + Math.random() * 900000).toString();
+        lottoNumbers.add(number);
+    }
+
+    return Array.from(lottoNumbers);
+};
+
+router.post('/start', async (req, res) => {
+    try {
+        const lottoNumbers = generateUniqueLottoNumbers();
+        res.json({ lottoNumbers });
+    } catch (error) {
+        res.status(500).send('Error generating lotto numbers');
     }
 });
 

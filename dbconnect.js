fae671681d3123fa3361fc06c636d29dfc8a68cf
campishome <campnapat007@gmail.com>
2024-09-13 19:19:29 +0700

    new db

--- dbconnect.js
+++ dbconnect.js
@@ -1,12 +1,19 @@
 const mysql = require('mysql2');
 
 
-const pool = mysql.createPool({
-    host: "202.28.34.197",
-    user: "web66_65011212009",
-    password: "65011212009@csmsu",
-    database: "web66_65011212009"
-});
+// const pool = mysql.createPool({
+//     host: "202.28.34.197",
+//     user: "web66_65011212009",
+//     password: "65011212009@csmsu",
+//     database: "web66_65011212009"
+// });
 
+const pool = mysql.createPool({
+    host: "localhost",
+    port: 8080,  
+    user: "cs",
+    password: "cs",
+    database: "mobile_project"
+});
 
 module.exports = pool.promise();

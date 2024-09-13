162baa0be4ccda0ec9f3b801e4085a2bf5922f6d
campishome <campnapat007@gmail.com>
2024-09-13 22:42:26 +0700

    fixed

--- dbconnect.js
+++ dbconnect.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 
 const pool = mysql.createPool({
-    host: "http://47.129.222.47/",
+    host: "202.28.34.197",
     user: "web66_65011212009",
     password: "65011212009@csmsu",
     database: "web66_65011212009"
